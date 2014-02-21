b5ff674c1cb78d5ab915779cc7a8596716d0e95b
honghu91 <honghu91@hotmail.com>
2014-02-21 20:13:49 +0800

    fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 dorsyClip
 =========
+
+####dorsyClip是一款强大的浏览器扩展工具，用于调试开发UI
+
+####dorsyClip已正式更名为AlloyDesigner
+
+##[AlloyDesigner](http://alloyteam.github.io/AlloyDesigner/)
+
+>感谢您的支持！
+
+
--- js/loader.js
+++
@@ -1,4 +0,0 @@
-var dorsyJs = ["main", "main.model", "main.util", "main.view", "main.eventBind", "main.css"];
-for(var i = 0; i < dorsyJs.length; i ++){
-    document.writeln("<script type='text/javascript' src='dorsyClip/js/" + dorsyJs[i] + ".js'></script>");
-}
